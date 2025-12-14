f71cfd3c6078ae875a7c543d8980d41ff141f24b
juliz-lab <julkucin@gmail.com>
2025-12-14 17:13:17 +0200

    Create main.cpp
    Įtrauktas main.cpp failas, kuriame bus iškviečiamos funkcijos ir vykdoma visa logika.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "mylib.h"
+
+int main()
+{
+    //SetConsoleOutputCP(CP_UTF8);
+    //SetConsoleCP(CP_UTF8); // lietuvisku raidziu isvedimui GALIMAI PAKEISTI REIKES
+
+    map<string, int> zodziai = failo_nuskaitymas("tekstas.txt");
+    failo_isvedimas(zodziai);
+}
