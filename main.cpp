0a28e14942556a3ed77f146baab975d526aa4268
juliz-lab <julkucin@gmail.com>
2025-12-14 19:51:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "mylib.h"
-
 int main()
 {
     //SetConsoleOutputCP(CP_UTF8);
     //SetConsoleCP(CP_UTF8); // lietuvisku raidziu isvedimui GALIMAI PAKEISTI REIKES
 
-    map<string, int> zodziai = failo_nuskaitymas("tekstas.txt");
+    map<string, zodzioInfo> zodziai = failo_nuskaitymas("tekstas.txt");
     failo_isvedimas(zodziai);
 }
