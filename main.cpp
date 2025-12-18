dab4d956b94ad3a39b85941efde8683bc9de8158
juliz-lab <julkucin@gmail.com>
2025-12-18 09:27:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mylib.h"
+
 int main()
 {
-    //SetConsoleOutputCP(CP_UTF8);
-    //SetConsoleCP(CP_UTF8); // lietuvisku raidziu isvedimui GALIMAI PAKEISTI REIKES
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8); // lietuvisku raidziu isvedimui terminale
 
     map<string, zodzioInfo> zodziai = failo_nuskaitymas("tekstas.txt");
     failo_isvedimas(zodziai);
+    vector<string> galai = url_nuskaitymas("url.txt");
+    vector<string> nuorodos = nuorodu_paieska("tekstas.txt", galai);
+    nuorodu_isvedimas(nuorodos);
 }
