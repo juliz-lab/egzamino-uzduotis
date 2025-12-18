9d96bc59538ca0505d3fd93d339b3947d782b091
juliz-lab <julkucin@gmail.com>
2025-12-18 09:31:00 +0200

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <windows.h>
 #include <vector>
+#include <cctype>
+#include <regex>
 
 using std::cout;
 using std::endl;
@@ -20,9 +22,7 @@ using std::tolower;
 using std::ofstream;
 using std::setw;
 using std::left;
-
-
-// using std::
+using std::regex;
 
 struct zodzioInfo{
     int kiekis = 0;
@@ -33,3 +33,8 @@ map<string, zodzioInfo> failo_nuskaitymas(const string &failas);
 bool ar_leistina_raide(string c);
 string naikinti_simbolius(string s);
 void failo_isvedimas(map<string, zodzioInfo> zodziai);
+vector<string> url_nuskaitymas(const string &failas);
+vector<string> nuorodu_paieska(const string &failas, const vector<string>& galai);
+void nuorodu_isvedimas(const vector<string> &nuorodos);
+bool regex_patikra(const string &zodis, const regex &r);
+regex regex_sablonas(const vector<string> &galai);
